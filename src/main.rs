a5c7df90cb084fb58c6124beee5dd30915f90338
winston <winstonlin@nsa.gove>
2021-08-02 13:14:37 +1000

    all done, except re_chan sender side gets dropped.

--- src/main.rs
+++ src/main.rs
@@ -39,18 +39,16 @@
 // }
 
 
-use async_std::task::{sleep, spawn};
 use futures::executor::block_on; // replaced with tokio's 
 use std::thread;
-use std::time;
 
 type Fork = i32;
 
 // msg sent by diners 
 struct Msg { // make it a package, .command: request or return, .which: the 2 forks that was taken
     cmd     : i32,          // 1 for retrieve, 2 for return 
-    who     : i32,          // which philosopher, 0 if sent by waiter
-    re_chan : Option<tokio::sync::mpsc::Sender<Msg>>,   // where i'm seated
+    who     : i32,          // which philosopher, 9 if sent by waiter
+    re_chan : Option<tokio::sync::mpsc::Sender<Msg>>,   // where i'm seated, only used by diner 
     forks   : (Option<Fork>, Option<Fork>)      // optional 
 }
 
@@ -72,9 +70,9 @@ async fn request(tx: tokio::sync::mpsc::Sender<Msg>, id: i32) -> (Fork, Fork) {
         re_chan : Some(send_side), 
         forks   : (None, None)
     };
-    // lord waiter, can i have two forks pls?
+    // can i have two forks? 
     tx.send(m).await;
-
+    
     let result: Option<Msg> = recv_side.recv().await;
 
     // this is SUPER ugly, fix it up later pls
@@ -82,23 +80,31 @@ async fn request(tx: tokio::sync::mpsc::Sender<Msg>, id: i32) -> (Fork, Fork) {
     match result {
         // implicit handling here results in either the inner wrapped value or panic
         Some(x) => ready_forks = (x.forks.0.unwrap_or(-1), x.forks.1.unwrap_or(-1)),  // we going 6-feet deep into hell if unwraps err, panic!
-        None    => println!("wtf how is this possible"),
+        None    => println!("how is this possible???"),
     };
 
-    println!("{}: got the forks i need", id);
+    println!("{}: got the forks ({}, {}) i need", id, ready_forks.0, ready_forks.1);
 
     ready_forks
 }
 
-async fn eat(forks: (Fork, Fork), id: i32) { 
+async fn eat(tx: tokio::sync::mpsc::Sender<Msg>, forks: (Fork, Fork), id: i32) { 
     println!("{}: Yum Yum, used forks {} and {}", id, forks.0, forks.1); 
 
-    // give back the forks
-    
+    // give back the forks to the waiter since i'm done eating
+    let m: Msg = Msg{ 
+        cmd     : 0, 
+        who     : id,
+        re_chan : None,
+        forks   : (Some(forks.0), Some(forks.1))
+    };
+    tx.send(m).await;
+
+    println!("{}: I have given the forks back", id);
 }
 
 async fn think(id: i32, deep_think: tokio::sync::mpsc::Receiver<bool>) { 
-    for i in 1..=50 {
+    for _i in 1..=50 {
         println!("{}: thinking", id);  // a little caveat, might handle later TODO, async callback/non-block msg passing, maybe poll_recv
         tokio::time::sleep(tokio::time::Duration::from_millis(50)).await;  
     }
@@ -108,23 +114,23 @@ async fn request_and_eat(tx: tokio::sync::mpsc::Sender<Msg>, id: i32, reminder: 
     // Wait until the song has been learned before singing it.
     // We use `.await` here rather than `block_on` to prevent blocking the
     // thread, which makes it possible to `dance` at the same time.
-    let (left_fork, right_fork) = request(tx, id).await; // yield control of the thread, tell the executor other futures can tun
+    let (left_fork, right_fork) = request(tx.clone(), id).await; // yield control of the thread, tell the executor other futures can tun
     // .await turns a future into a value of specified type 
 
-    eat((left_fork, right_fork), id).await;
+    eat(tx, (left_fork, right_fork), id).await;
 }
 
 async fn async_main(tx: tokio::sync::mpsc::Sender<Msg>) { 
     loop {
         // first philospher 
         let (reminder, mut deep_think): (tokio::sync::mpsc::Sender<bool>, tokio::sync::mpsc::Receiver<bool>) = tokio::sync::mpsc::channel(1);
-        let f1 = request_and_eat(tx.clone(), 1, reminder); // this is where the actual 'async' magic takes place
-        let f2 = think(1, deep_think);
-
-        let (reminder, mut deep_think): (tokio::sync::mpsc::Sender<bool>, tokio::sync::mpsc::Receiver<bool>) = tokio::sync::mpsc::channel(1);
-        let f3 = request_and_eat(tx.clone(), 2, reminder); // this is where the actual 'async' magic takes place
-        let f4 = think(2, deep_think);
+        let f1 = request_and_eat(tx.clone(), 0, reminder); // this is where the actual 'async' magic takes place
+        let f2 = think(0, deep_think);
         
+        let (reminder, mut deep_think): (tokio::sync::mpsc::Sender<bool>, tokio::sync::mpsc::Receiver<bool>) = tokio::sync::mpsc::channel(1);
+        let f3 = request_and_eat(tx.clone(), 1, reminder); // this is where the actual 'async' magic takes place
+        let f4 = think(1, deep_think);
+
         // more...
 
         // `join!` is like `.await` but can wait for multiple futures concurrently.
@@ -140,9 +146,7 @@ async fn async_main(tx: tokio::sync::mpsc::Sender<Msg>) {
 }
 
 fn main() {
-    let rt = tokio::runtime::Runtime::new().unwrap();
-
-    let mut forks = [0; 5]; // used by the waiter
+    let mut forks = [1; 5]; // used by the waiter
 
     // a channel for asking forks and returning forks
     // sync <-> async / async <-> async, bounded channel 
@@ -154,22 +158,53 @@ fn main() {
         loop {
             let m = rx.blocking_recv().unwrap(); // blocking synchronously, sent from an async task
 
-            println!("Waiter: {} received a request", m.who);
+            if m.cmd == 1 { // standard dining philosopher 
+                println!("Waiter: got a request from {}", m.who);
 
-            if m.cmd == 1 {
-                // look at forks and decide
-                // standard dining philo here
-                let r: Msg = Msg{ 
-                    cmd     : 1, 
-                    who     : 0,
-                    re_chan : None, 
-                    forks   : (Some(3), Some(4))
-                };
-                m.re_chan.unwrap().blocking_send(r);
+                // impose total ordering of the resources, each philosopher
+                // are to request the forks in order 
+                let mut first_fork = -1;
+                let mut second_fork = -1;
+                let position = m.who; // philisophers are indexed 0..4, 5 in total
+                if position < 4 {
+                    first_fork = position;
+                    second_fork = position+1;
+                }
+                else if position == 4 {
+                    first_fork = 0;
+                    second_fork = 4;
+                }
+
+                if forks[first_fork as usize] == 1 && forks[second_fork as usize] == 1 { // both forks requested are available
+                    let r: Msg = Msg { 
+                        cmd     : 1, 
+                        who     : 0,
+                        re_chan : None, 
+                        forks   : (Some(first_fork), Some(second_fork))
+                    };
+                    
+                    println!("Waiter: forks ({}, {}) given to {}", first_fork, second_fork, m.who);
+                    m.re_chan.unwrap().blocking_send(r);
+
+                    forks[first_fork as usize] = 0;
+                    forks[second_fork as usize] = 0;
+                }
+                // else { // forks not available, keep thinking!
+                //     let r: Msg = Msg { 
+                //         cmd     : 1, 
+                //         who     : 0,
+                //         re_chan : None, 
+                //         forks   : (None, None)
+                //     };
+                    
+                     println!("Waiter: sorry, the forks({}, {}) you want aren't available", first_fork, second_fork);
+                //     m.re_chan.unwrap().blocking_send(r);
+                // }
             } 
-            else if m.cmd == 2 { // returns
-                forks[m.forks.0.unwrap() as usize] = 0;
-                forks[m.forks.1.unwrap() as usize] = 0;
+            else if m.cmd == 2 { // diner returning forks 
+                forks[m.forks.0.unwrap() as usize] = 1;
+                forks[m.forks.1.unwrap() as usize] = 1;
+                println!("Waiter: got back forks from {}", m.who);
             }
         }
     });
@@ -177,22 +212,28 @@ fn main() {
     // `block_on` blocks the current thread until the provided future has run to
     // completion. Other executors provide more complex behavior, like scheduling
     // multiple futures onto the same thread.
-
+    // spawning a future 'thread' and immediately joining it
+    let rt = tokio::runtime::Runtime::new().unwrap();
     rt.block_on(async {
-        async_main(tx).await;
+        async_main(tx.clone()).await;
     });
-    //Spawn a future onto the runtime
-    
-    // let rt_handle1 = rt.handle();
-    // let rt_handle2 = rt.handle();
-    // let rt_handle3 = rt.handle();
-    // let rt_handle4 = rt.handle();
-    // let rt_handle5 = rt.handle();
+
+    // let tx2 = tx.clone();
+    // let tx3 = tx.clone();
 
     // thread::spawn(move || {
-    //     println!("started philo #1");
-    //     rt.handle().clone().block_on(async {
-    //         async_main(tx).await;
+    //     let rt = tokio::runtime::Runtime::new().unwrap();
+    //     rt.block_on(async {
+    //         async_main(tx2.clone()).await;
+    //     });
+    // });
+    // println!("started 1th philosopher");
+
+    // thread::spawn( move || {
+    //     println!("started 2th philosopher");
+    //     let rt = tokio::runtime::Runtime::new().unwrap();
+    //     rt.block_on(async {
+    //         async_main(tx3.clone()).await;
     //     });
     // });
 }
